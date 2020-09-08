bafd9243a510af602931f5455444b1c6f3933785
kristal374 <70152861+kristal374@users.noreply.github.com>
2020-09-08 16:12:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(('local host', 53210))
+c.connect(('localhost', 53210))
 print('local host')
 c.sendall(b'Hello, world')
 data = c.recv(1024)
